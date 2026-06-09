222a2380d6ac4dc9f9f9174c84e68e463a218e2e
Hlengiwe-debug <hlentunja@gmail.com>
2026-06-09 07:22:22 +0000

    tutorial 2

---
+++ tutorial_2/main.java
@@ -0,0 +1,10 @@
+public class main  {
+    public static void main(String[] args)  {
+        // coding starts here;
+
+        Student s1= new Student("Ashley",21,4.0);
+        s1.displayInfo();
+        s1.takeExam();
+        s1.study();
+    }
+}
